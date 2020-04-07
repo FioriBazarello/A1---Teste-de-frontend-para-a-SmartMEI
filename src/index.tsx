4bef57853b195c0db5f2b512f84710e9a9524451
Fiori Bazarello <fioribazarello@gmail.com>
2020-04-07 08:50:52 -0300

    Remoção de arquivos e bibliotecas não usados

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
-
 import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'https://api.graphql.jobs/',
-})
-
-const client = new ApolloClient({
-  cache,
-  link,
-})
-
 const AppComponent = () => {
   return (
-    <ApolloProvider client={client}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ApolloProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   );
 }
 
